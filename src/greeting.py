73418be95cb25c3533e546ce5bfc8b6ea5fbf2ed
eesahee <isaimorfin@gmail.com>
2024-05-05 11:26:59 -0600

    Update greeting.py

--- src/greeting.py
+++ src/greeting.py
@@ -1,18 +1,17 @@
 import os
+
+
 def greeting():
+    # Get the value of the ENVIRONMENT_VARIABLE environment variable, defaulting to None if not set
+    environment_value = os.environ.get('ENVIRONMENT_VARIABLE')
+
+    # Check if the environment variable is set
+    if environment_value is not None:
+        print(environment_value)
+    else:
+        print("Hello World")
 
 
-# Get the value of the ENVIRONMENT_VARIABLE environment variable, defaulting to None if not set
-environment_value = os.environ.get('ENVIRONMENT_VARIABLE')
-
-# Check if the environment variable is set
-if environment_value is not None:
-    print(environment_value)
-else:
-    print("Hello World")
-
-    # make sure only main.py is entry point
-
-
+# Make sure only main.py is the entry point
 if __name__ == "__main__":
     print("This script should not be run directly. Please run main.py instead.")
