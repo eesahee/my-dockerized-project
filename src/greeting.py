22ce711b618d95ce12e704a70f93d7ca03256a6e
eesahee <isaimorfin@gmail.com>
2024-05-05 11:20:59 -0600

    Update greeting.py
    pass env variables to docker container

--- src/greeting.py
+++ src/greeting.py
@@ -1,4 +1,14 @@
+import os
 def greeting():
+
+
+# Get the value of the ENVIRONMENT_VARIABLE environment variable, defaulting to None if not set
+environment_value = os.environ.get('ENVIRONMENT_VARIABLE')
+
+# Check if the environment variable is set
+if environment_value is not None:
+    print(environment_value)
+else:
     print("Hello World")
 
     # make sure only main.py is entry point
